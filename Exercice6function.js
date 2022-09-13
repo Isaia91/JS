06811aec4f0c4ffc408b17e80cc4249a1c95d588
Isaia91 <112839257+Isaia91@users.noreply.github.com>
2022-09-13 11:32:04 +1100

    test2

---
+++ Exercice6function.js
@@ -0,0 +1,7 @@
+ function Reste(){
+    var nombre1= parseInt(prompt("Quel est ton premier nombre a virgule?"));
+    var nombre2= parseFloat(prompt("Quel est ton second nombre a virgule?"));
+    r=nombre1%nombre2;
+    alert(r);
+}
+Reste();
